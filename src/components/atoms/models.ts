282cec89a05fa4d9a63ad55c28887e1c10eb8034
aryandutt <aryandutt21@gmail.com>
2024-03-05 13:35:39 +0530

    testing deployment

--- src/components/atoms/models.ts
+++ src/components/atoms/models.ts
@@ -6,11 +6,8 @@ const modelAtomSelector = selector({
   get: async () => {
     try {
       const headers = new Headers();
-      headers.append(
-        "X-Master-Key",
-        process.env.NEXT_PUBLIC_API_KEY as string
-      );
-      headers.append("X-JSON-Path", '$.models.*');
+      headers.append("X-Master-Key", process.env.NEXT_PUBLIC_API_KEY as string);
+      headers.append("X-JSON-Path", "$.models.*");
       const response = await fetch(
         `https://api.jsonbin.io/v3/b/${process.env.NEXT_PUBLIC_BIN_ID}?meta=false`,
         {
@@ -34,5 +31,16 @@ const modelAtomSelector = selector({
 
 export const modelAtom = atom<ModelInterface[]>({
   key: "modelAtom1",
-  default: modelAtomSelector || [],
+  // default: modelAtomSelector || [],
+  default: [
+    {
+      author: "BigCode",
+      avatarUrl:
+        "https://aeiljuispo.cloudimg.io/v7/https://cdn-uploads.huggingface.co/production/uploads/1659521200179-5e48005437cb5b49818287a5.png?w=200&h=200&f=face",
+      downloads: 2427,
+      likes: 247,
+      tag: "text-generation",
+      model: "starcoder2-15b",
+    },
+  ],
 });
